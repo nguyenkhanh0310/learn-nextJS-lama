429049c97c8854bdc3cca671cc3a2b94746f28e1
nguyenkhanh0310 <mykhanh03102003@gmail.com>
2024-11-19 11:14:49 +0700

    NextJS App Route, Llayouts, Components, Loading, Error, Not Found Layouts

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import Links from "@/components/navbar/links/Links";
+import React from "react";
+
+const Navbar = () => {
+
+  return (
+    <>
+      <div>Logo</div>
+      <div>
+        <Links/>
+      </div>
+    </>
+  )
+};
+
+export default Navbar;
---
+++ src/components/navbar/links/Links.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Links() {
+
+  const links = [
+    {
+      title: "Homepage",
+      path: "/"
+    },
+    {
+      title: "About",
+      path: "/about"
+    },
+    {
+      title: "Contact",
+      path: "/contact"
+    },
+    {
+      title: "Blog",
+      path: "/blog"
+    },
+  ]
+
+  return (
+    <>
+      {links.map((link) => (
+        <Link href={link.path} key={link.title}>{link.title}</Link>
+      ))}
+    </>
+  )
+}
