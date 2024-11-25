3422f9a4c2f1e8abbb4580f8bb9167e90d172ec1
nguyenkhanh0310 <mykhanh03102003@gmail.com>
2024-11-25 08:54:41 +0700

    Hydration error and solution

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,25 @@
+"use client"
 import Image from "next/image";
 import styles from  "./contact.module.css"
-import React from 'react'
+import dynamic from "next/dynamic";
+// import HydrationTest from "@/components/hydrationTest";
+// import React, { useEffect, useState } from 'react'
 
+const HydrationTestNoSSR = dynamic(()=>import("@/components/hydrationTest"), {ssr: false})
 const ContactPage = () => {
+  // const [ isClient, setIsClient] = useState(false);
+
+  // useEffect(() => {
+  //   setIsClient(true);
+  // }, []);
+
+  // const a = Math.random(); 
+  // console.log(a);
+
+  // const a = Math.random();
+
+  // console.log(a);
+  
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
@@ -14,6 +31,9 @@ const ContactPage = () => {
         />
       </div>
       <div className={styles.formContainer}>
+        {/* {isClient && <div>{a}</div>} */}
+        <HydrationTestNoSSR/>
+        {/* <div suppressHydrationWarning>{a}</div> */}
         <form action="" className={styles.form}>
           <input type="text" placeholder="Name and Surname"/>
           <input type="text" placeholder="Email Address"/>
